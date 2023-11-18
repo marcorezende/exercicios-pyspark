78f9b798a3b5330b2b275afb710468d10da78fe6
Marco Rezende <marco.rezende@fpf.br>
2023-11-18 12:59:51 -0400

    init

--- test.py
+++ test.py
@@ -4,22 +4,18 @@ spark = SparkSession.builder \
     .getOrCreate()
 
 
-class VerificaoExercicio:
-
-    @staticmethod
-    def checar(sample, df):
-        try:
-            empty = sample.subtract(df).isEmpty()
-            if empty:
-                print("Você acertou!")
-            else:
-                print("Você errou!")
-        except Exception as e:
+def checar(sample, df):
+    try:
+        empty = sample.subtract(df).isEmpty()
+        if empty:
+            print("Você acertou!")
+        else:
             print("Você errou!")
-
-    def exercicio(self, numero, df):
-        path = f'./arquivos/exercicios/exercicio{numero}'
-        sample = spark.read.parquet(path)
-        self.checar(sample, df)
+    except Exception as e:
+        print("Você errou!")
 
 
+def testar(numero, df):
+    path = f'/content/exercicios-pyspark/arquivos/exercicios/exercicio{numero}'
+    sample = spark.read.parquet(path)
+    checar(sample, df)
